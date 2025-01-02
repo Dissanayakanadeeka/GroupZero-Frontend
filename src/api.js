d9691ab1623c1d85fc7c63dd060cb9457faad098
Milinda Sachinthaka <e20286@eng.pdn.ac.lk>
2025-01-03 02:36:44 +0530

    setup member form in home

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 
-const apiUrl = "https://group-zero-d291f139ee8c.herokuapp.com";
+const apiUrl = "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import api from '../api';
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const [isMember, setIsMember] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    checkIsMember();
+  }, []);
+
+  async function checkIsMember() {
+    try {
+      const response = await api.get("/api/is_member/");
+      if (response.data.length !== 0) {
+        setIsMember(true);
+      }
+    } catch (error) {
+      console.error("Error checking membership:", error);
+      alert("Failed to check membership. Please try again later.");
+    }
+  }
+
+  function handle_logout() {
+    navigate("/logout");
+  }
   return (
     <div>
-      <h1>Home</h1>
+      {isMember && 
+        <div>
+          <h1>Welcome Home</h1>
+          <p>Thank you for being a member!</p>
+          <button onClick={handle_logout}>Logout</button>
+        </div>
+      }
+      {!isMember && 
+      <dev>
+        <h3>fill following form to become a member</h3>
+        <button onClick={handle_logout}>Logout</button>
+      </dev>
+      }
     </div>
   );
 }
